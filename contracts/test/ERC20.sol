5fa43bbe8918032e23c16dc1a2ffdc80d904ddbc
Alexander Petree <apetree1001@email.phoenix.edu>
2023-12-30 22:23:43 -0800

    Update ERC20.sol
    Signed-off-by: Alexander Petree <apetree1001@email.phoenix.edu>

--- contracts/test/ERC20.sol
+++ contracts/test/ERC20.sol
@@ -1,9 +1,3 @@
 pragma solidity =0.5.16;
-
 import '../CroDefiSwapERC20.sol';
-
-contract ERC20 is CroDefiSwapERC20 {
-    constructor(uint _totalSupply) public {
-        _mint(msg.sender, _totalSupply);
-    }
-}
+contract ERC20 is CroDefiSwapERC20 {constructor(uint _totalSupply) public { _mint(msg.sender, _totalSupply);}}
